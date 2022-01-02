093efcb97ca9c7ae289d071139d70e964c8b8bc0
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2022-01-02 14:43:02 +0900

    Initial Commit

---
+++ railroad-switch macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  railroad-switch macOS
+//
+//  Created by Rintaro Kawagishi on 02/01/2022.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
