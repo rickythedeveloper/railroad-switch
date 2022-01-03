8280cb07d5670afaf22f00753225074de929e740
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2022-01-03 16:20:07 +0900

    #9 remove bits from template. Close #9

--- railroad-switch Shared/GameScene.swift
+++ railroad-switch Shared/GameScene.swift
@@ -29,37 +29,6 @@ class GameScene: SKScene {
     }
     
     func setUpScene() {
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 4.0
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-            
-            #if os(watchOS)
-                // For watch we just periodically create one of these and let it spin
-                // For other platforms we let user touch/mouse events create these
-                spinnyNode.position = CGPoint(x: 0.0, y: 0.0)
-                spinnyNode.strokeColor = SKColor.red
-                self.run(SKAction.repeatForever(SKAction.sequence([SKAction.wait(forDuration: 2.0),
-                                                                   SKAction.run({
-                                                                       let n = spinnyNode.copy() as! SKShapeNode
-                                                                       self.addChild(n)
-                                                                   })])))
-            #endif
-        }
-        
         self.gameEngine = GameEngine(moveTrainInScene: self.moveTrain, pauseTrainInScene: self.pauseTrain)
         self.addJoint(position: CGPoint(x: -200, y: 0))
         self.addJoint(position: CGPoint(x: 0, y: 0))
@@ -86,14 +55,6 @@ class GameScene: SKScene {
         self.setUpScene()
     }
     #endif
-
-    func makeSpinny(at pos: CGPoint, color: SKColor) {
-        if let spinny = self.spinnyNode?.copy() as! SKShapeNode? {
-            spinny.position = pos
-            spinny.strokeColor = color
-            self.addChild(spinny)
-        }
-    }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
@@ -127,72 +88,32 @@ class GameScene: SKScene {
         for joint in self.gameEngine.joints { self.addChild(joint.skNode) }
         for train in self.gameEngine.trains { self.addChild(train.skNode) }
     }
+    
+    func respondToTouch(location: CGPoint) {
+        let touchedNodes = self.nodes(at: location)
+        for n in touchedNodes {
+            let tracks = self.gameEngine.tracks.filter { track in track.skNode == n }
+            tracks.forEach { track in self.gameEngine.switchTrack(track) }
+            
+            let trains = self.gameEngine.trains.filter { train in train.skNode == n }
+            for train in trains { self.gameEngine.pauseResumeTrain(train) }
+        }
+    }
 }
 
 #if os(iOS) || os(tvOS)
 // Touch-based event handling
 extension GameScene {
-
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches {
-            self.makeSpinny(at: t.location(in: self), color: SKColor.green)
-            
-            let touchedNodes = self.nodes(at: t.location(in: self))
-            for n in touchedNodes {
-                let tracks = self.gameEngine.tracks.filter { track in track.skNode == n }
-                tracks.forEach { track in self.gameEngine.switchTrack(track) }
-                
-                let trains = self.gameEngine.trains.filter { train in train.skNode == n }
-                for train in trains { self.gameEngine.pauseResumeTrain(train) }
-            }
-        }
+        for t in touches { self.respondToTouch(location: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            self.makeSpinny(at: t.location(in: self), color: SKColor.blue)
-        }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            self.makeSpinny(at: t.location(in: self), color: SKColor.red)
-        }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches {
-            self.makeSpinny(at: t.location(in: self), color: SKColor.red)
-        }
-    }
-    
-   
 }
 #endif
 
 #if os(OSX)
 // Mouse-based event handling
 extension GameScene {
-
-    override func mouseDown(with event: NSEvent) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        self.makeSpinny(at: event.location(in: self), color: SKColor.green)
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.makeSpinny(at: event.location(in: self), color: SKColor.blue)
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.makeSpinny(at: event.location(in: self), color: SKColor.red)
-    }
-
+    override func mouseDown(with event: NSEvent) { self.respondToTouch(location: event.location(in: self)) }
 }
 #endif
 
